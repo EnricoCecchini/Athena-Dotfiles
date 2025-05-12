59c1776735f5b39a70eecc35b79c9d62c6719b24
EnricoCecchini <enrico.cecchini243@gmail.com>
2025-05-12 21:33:11 +0000

    Updated setup script to install new apps

--- setup.sh
+++ setup.sh
@@ -16,8 +16,11 @@ echo "Beginning Setup"
 
 # Install recquired packages
 echo "Installing packages..."
-sudo packman -S kitty fish rofi stow thunar github-cli
+sudo pacman -S neovim kitty fish rofi stow thunar github-cli steam
 
+# Intstall NVIDIA Utils
+echo "Installing Nvidia Utils..."
+sudo pacman -S lib32-nvidia-utils
 
 # Link config files
 echo "Stowing config files..."
@@ -30,21 +33,48 @@ if [ "$SHELL" != "/usr/bin/fish" ]; then
     echo "Default shell changed to fish. Log out and back in to apply."
 fi
 
+# Reload Terminal
+exec fish
+
 # Install Utilities
 echo "Installing Utilities"
 echo "Making utils folder"
-mkdir ~/Utils
+mkdir -p ~/Utils
 
 # Installing yay
-echo "Installing yay"
+echo "Installing AUR helper `yay`"
 cd ~/Utils
 git clone https://aur.archlinux.org/yay.git
 cd ~/Utils/yay
-makepkg -sri
+makepkg -sri --noconfirm
 
 # Install VS Code
 echo "Installing VS Code"
 cd ~/Utils
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd ~/Utils/visual-studio-code-bin
-makepkg -sri
+makepkg -sri --noconfirm
+
+# Install librewolf
+flatpak install flathub io.gitlab.librewolf-community
+
+# Install VLC
+flatpak install flathub org.videolan.VLC
+
+# Install Spotify
+flatpak install flathub com.spotify.Client
+
+# Install Bottles
+flatpak install flathub com.usebottles.bottles
+
+# Install Libre Office
+flatpak install flathub org.libreoffice.LibreOffice
+
+# Install Plex Desktop Client
+flatpak install flathub tv.plex.PlexDesktop
+
+# Install LACT (GPU Manager)
+flatpak install flathub io.github.ilya_zlobintsev.LACT
+
+# Install Beekeeper
+flatpak install flathub io.beekeeperstudio.Studio
